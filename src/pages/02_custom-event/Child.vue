c4792c5eb4a1dff900d64b82e9b93ff6ede92a0c
BAOfanTing <1316149505@qq.com>
2026-02-28 14:12:18 +0800

    组件通信_自定义事件

--- src/pages/02_custom-event/Child.vue
+++ src/pages/02_custom-event/Child.vue
@@ -4,19 +4,24 @@
 	<h2>儿子的玩具是{{toy}}</h2>
 	<h2>父亲的车是{{car}}</h2>
 	<button @click="send">给玩具</button>
+	<button @click="handleClick($event)">点击</button>
   </div>
 </template>
 
 <script setup lang="ts" name="Child">
-import {ref,defineEmits} from 'vue';
+import {ref} from 'vue';
 
 let toy = ref('巴斯光年');
-
+//要定义在函数外，否则在函数中会报错，因为函数中没有this指向
+const emit = defineEmits(['send-toy']);
+function handleClick(event:any) {
+  console.log(event) // MouseEvent 对象
+  console.log(event.target) // 触发事件的元素
+}
 function send(){
-	const emit = defineEmits(['send-toy']);
+	//发送事件,同时发送需要传递的参数
 	emit('send-toy',toy);
 }
-
 //接受父组件的props
 defineProps(['car']);
 </script>
--- src/pages/02_custom-event/Father.vue
+++ src/pages/02_custom-event/Father.vue
@@ -3,6 +3,7 @@
     <h3>父组件</h3>
 	<h2>父亲的车是{{ car }}</h2>
 	<h2>儿子给父亲的玩具{{toy}}</h2>
+	<!--当子组件的发送send-toy信号时，会触发getToy函数-->
 	<Child @send-toy="getToy"></Child>
   </div>
 </template>
